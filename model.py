de7bbb6b4df5d7a4a744218c6de93eb21a6ec14a
Zishen Chen <32064967+jasonchan117@users.noreply.github.com>
2021-03-17 14:11:04 +0800

    Architecture of the model.

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ class RNN(nn.Module):
         self.rnn = nn.LSTM(embedding_dim, hidden_dim, num_layers=n_layers,
                            bidirectional=bidirectional, dropout=dropout)
 
-        self.fc = nn.Linear(hidden_dim * 2 if bidirectional==True else 1, output_dim)  # *2是因为BiLSTM
+        self.fc = nn.Linear(hidden_dim * 2 if bidirectional==True else 1, output_dim)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, text):
@@ -18,7 +18,7 @@ class RNN(nn.Module):
 
         output, (hidden, cell) = self.rnn(embedded)
 
-        hidden = self.dropout(torch.cat((hidden[-2, :, :], hidden[-1, :, :]), dim=1))#64*512
+        hidden = self.dropout(torch.cat((hidden[-2, :, :], hidden[-1, :, :]), dim=1))
 
         return self.fc(hidden)
 
@@ -27,13 +27,14 @@ class HPC(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim,
                  n_layers, bidirectional, dropout, pad_idx):
         super().__init__()
-        self.rnn= RNN(vocab_size, embedding_dim, hidden_dim, output_dim,
+        self.agency= RNN(vocab_size, embedding_dim, hidden_dim, output_dim,
+                 n_layers, bidirectional, dropout, pad_idx)
+        self.social = RNN(vocab_size, embedding_dim, hidden_dim, output_dim,
                  n_layers, bidirectional, dropout, pad_idx)
     def forward(self, text):
         embedded = self.dropout(self.embedding(text))
 
-        output, (hidden, cell) = self.rnn(embedded)
+        output_a = self.agency(embedded)
+        output_s = self.social(embedded)
 
-        hidden = self.dropout(torch.cat((hidden[-2, :, :], hidden[-1, :, :]), dim=1))#64*512
-
-        return self.fc(hidden)
+        return output_a, output_s
